d57ecd4053f4262320f6384a475df4d3677ef833
rlaengud123 <53484692+rlaengud123@users.noreply.github.com>
2020-02-27 16:59:27 +0900

    Add files via upload

--- LRCN/lrcn_model.py
+++ LRCN/lrcn_model.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 from torchvision import models
 
 class ConvLstm(nn.Module):
-    def __init__(self, latent_dim, hidden_size, lstm_layers, bidirectional, n_class):
+    def __init__(self, latent_dim, model, hidden_size, lstm_layers, bidirectional, n_class):
         super(ConvLstm, self).__init__()
-        self.conv_model = Pretrained_conv(latent_dim)
+        self.conv_model = Pretrained_conv(latent_dim, model)
         self.Lstm = Lstm(latent_dim, hidden_size, lstm_layers, bidirectional)
         self.output_layer = nn.Sequential(
             nn.Linear(2 * hidden_size if bidirectional==True else hidden_size, n_class),
@@ -22,14 +22,31 @@ class ConvLstm(nn.Module):
         return output
 
 class Pretrained_conv(nn.Module):
-    def __init__(self, latent_dim):
-        super(Pretrained_conv, self).__init__()
-        self.conv_model = models.resnet152(pretrained=True)
-        # ====== freezing all of the layers ======
-        for param in self.conv_model.parameters():
-            param.requires_grad = False
-        # ====== changing the last FC layer to an output with the size we need. this layer is un freezed ======
-        self.conv_model.fc = nn.Linear(self.conv_model.fc.in_features, latent_dim)
+    def __init__(self, latent_dim, model):
+        if model == 'resnet152':
+            super(Pretrained_conv, self).__init__()
+            self.conv_model = models.resnet152(pretrained=True)
+            # ====== freezing all of the layers ======
+            for param in self.conv_model.parameters():
+                param.requires_grad = False
+            # ====== changing the last FC layer to an output with the size we need. this layer is un freezed ======
+            self.conv_model.fc = nn.Linear(self.conv_model.fc.in_features, latent_dim)
+        elif model == 'densenet201':
+            super(Pretrained_conv, self).__init__()
+            self.conv_model = models.densenet201(pretrained=True)
+            # ====== freezing all of the layers ======
+            for param in self.conv_model.parameters():
+                param.requires_grad = False
+            # ====== changing the last FC layer to an output with the size we need. this layer is un freezed ======
+            self.conv_model.classifier = nn.Linear(self.conv_model.classifier.in_features, latent_dim)
+        elif model == 'densenet161':
+            super(Pretrained_conv, self).__init__()
+            self.conv_model = models.densenet161(pretrained=True)
+            # ====== freezing all of the layers ======
+            for param in self.conv_model.parameters():
+                param.requires_grad = False
+            # ====== changing the last FC layer to an output with the size we need. this layer is un freezed ======
+            self.conv_model.classifier = nn.Linear(self.conv_model.classifier.in_features, latent_dim)
 
     def forward(self, x):
         return self.conv_model(x)
